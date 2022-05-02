6b620c5b56caa26a1797016d3044f20c3c2be400
Catagzzi <catagzzi@gmail.com>
2022-05-01 21:01:17 -0400

    Adds functionality to the endpoint that obtains the referral links

--- src/components/referral-form.js
+++ src/components/referral-form.js
@@ -3,32 +3,41 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
-const initialFormData = Object.freeze({
-    email: "",
-    name: "",
-  });
+export default function BasicTextFields(props) {
+  const [email, updateEmail] = React.useState();
+  const [name, updateName] = React.useState();
 
+  const handleChangeEmail = (e) => {
+    console.log("BBBBBB", e.target.value)
+    updateEmail(e.target.value);
+    console.log("handleChangeEmail", email)
+  };
 
-
-export default function BasicTextFields() {
-    const [email, updateEmail] = React.useState();
-    const [name, updateName] = React.useState();
-    const handleChangeEmail = (e) => {
-        console.log("BBBBBB", e.target.value)
-        updateEmail(e.target.value);
-        console.log(email)
-      };
-    const handleChangeName = (e) => {
+  const handleChangeName = (e) => {
     console.log("BBBBBB", e.target.value)
     updateName(e.target.value);
     console.log(name)
-    };
-    const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('enviando datos...' + name + ' ' + email)
-    // ... submit to API or something
-    };
+  };
+
+  function Greeting(props) {
+    const success = props.success;
+    const link = props.link;
+    if (success === 200) {
+      let referralLink = <a href={link}> {link}</a>;
+      return <Grid style={{ width:"100%"}}> Tu link de referido es: {referralLink} </Grid>;
+    }
+    if (success === 404) {
+      let aqui = <a href={"http://localhost:3050/users/referral/create"}> aquí</a>;
+      return <Grid style={{ width:"100%"}}>Usuario no encontrado. Por favor escriba un mail correcto. <br/> Si usted no se ha registrado, por favor hágalo {aqui}. </Grid>
+    }
+    if (success === 500) {
+      return <Grid style={{ width:"100%"}}>Surgió un error al obtener el link de referido, por favor inténtelo de nuevo</Grid>
+    }
+    return <p></p>;
+  }
+
   return (
     <Box
       component="form"
@@ -46,16 +55,12 @@ export default function BasicTextFields() {
             </p>
         </Typography>
         <br/>
-        <TextField name="email" label="Email" variant="outlined"  onChange={handleChangeEmail}/>
-        <TextField name="name" label="Nombre Completo" variant="outlined"  onChange={handleChangeName}/>
+        <TextField required name="email" label="Email" variant="outlined"  onChange={handleChangeEmail} />
+        <TextField required name="name" label="Nombre Completo" variant="outlined"  onChange={handleChangeName}/>
         <br/>
-        <Button id="referral-link-button" variant="contained" onClick={handleSubmit}>Compartir</Button>
-        <li>
-            {name}
-        </li>
-        <li>
-            {   email}
-        </li>
+        <Button id="referral-link-button" variant="contained" onClick={() => props.getLink(email)}>Compartir</Button>
+        <br/>
+        <Greeting success={props.resCode} link={props.resLink}></Greeting>
     </Box>
   );
 }
--- src/pages/referral.js
+++ src/pages/referral.js
@@ -1,26 +1,43 @@
-
+import React, {useState} from 'react'
 import Header from '../components/header'
 import ReferralForm from '../components/referral-form'
 import Grid from '@mui/material/Grid'
 
 const Referral = () =>   {
-    return (
-      <div className="App">
-        <div >
-          <Header />
-        </div >
-          <Grid
-            container justify="center" direction="column"
-            alignItems="center"
-          >
-            <Grid
-              container justify="center"
-              alignItems="center"
-              style={{ minHeight: "90vh", width:"50%"}}>
-              <ReferralForm />
-            </Grid >
-          </Grid>
-      </div>
-    );
+  const [response, setResponse] = useState("")
+
+  function getLink(email) {
+    fetch('http://localhost:3050/users/referral/create', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: email,
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+          setResponse(data)
+        });
   }
-  export default Referral;
+
+  return (
+    <div className="App">
+      <div >
+        <Header />
+      </div >
+        <Grid
+          container justify="center" direction="column"
+          alignItems="center"
+        >
+          <Grid
+            container justify="center"
+            alignItems="center"
+            style={{ minHeight: "90vh", width:"50%"}}>
+            <ReferralForm getLink={getLink} resCode={response.code} resMsg={response.message} resLink={response.result}/>
+          </Grid >
+        </Grid>
+    </div>
+  );
+}
+
+export default Referral;
